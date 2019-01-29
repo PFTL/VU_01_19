c14f8e4997c558073115dfc5a0d70d9f562ec209
Aquiles Carattino <aquiles@aquicarattino.com>
2019-01-29 21:47:48 +0100

    Cleaned up the directory

--- simple_daq.py
+++ simple_daq.py
@@ -1,53 +1,56 @@
 import serial
 from time import sleep
 
-class Device:
-    def __init__(self, port):
-        self.port = port
-        self.rsc = None
 
-    def initialize(self):
-        self.rsc = serial.Serial(self.port)
+class SimpleDaq:
+    DEFAULTS = {'write_termination': '\n',
+                'read_termination': '\n',
+                'encoding': 'ascii',
+                'baudrate': 9600,
+                'write_timeout': 1,
+                'read_timeout': 1,
+                }
+    rsc = None
+
+    def __init__(self, port):
+        self.initialize(port)
+
+    def initialize(self, port):
+        self.rsc = serial.Serial(port=port,
+                                 baudrate=self.DEFAULTS['baudrate'],
+                                 timeout=self.DEFAULTS['read_timeout'],
+                                 write_timeout=self.DEFAULTS['write_timeout'])
         sleep(0.5)
 
     def idn(self):
-        """Get the identification string of the device"""
-        return self.query('IDN')
+        return self.query("IDN")
 
-    def read_analog(self, channel):
-        command = 'IN:CH{}'.format(channel)
-        return int(self.query(command))
+    def get_analog_value(self, channel):
+        query_string = 'IN:CH{}'.format(channel)
+        value = int(self.query(query_string))
+        return value
 
-    def set_analog(self, channel, value):
-        if value > 4095:
-            raise Exception('Value cant exceed 4095')
+    def set_analog_value(self, channel, value):
+        write_string = 'OUT:CH{}:{}'.format(channel, value)
+        self.write(write_string)
 
-        command = 'OUT:CH{}:{}'.format(channel, value)
-        self.write(command)
+    def finalize(self):
+        if self.rsc is not None:
+            self.rsc.close()
+
+    def query(self, message):
+        self.write(message)
+        return self.read()
+
+    def write(self, message):
+        if self.rsc is None:
+            raise Warning("Trying to write to device before initializing")
+
+        msg = (message + self.DEFAULTS['write_termination']).encode(self.DEFAULTS['encoding'])
+        self.rsc.write(msg)
         sleep(0.1)
 
-    def write(self, command):
-        if self.rsc is None:
-            raise Exception('Device not initialized. Please run initialize first.')
-
-        command = command + '\n'
-        command = command.encode('ascii')
-        self.rsc.write(command)
-
-    def query(self, command):
-        self.write(command)
-        return self.rsc.readline().decode('ascii').strip()
-
-if __name__ == '__main__':
-    dev = Device('/dev/ttyACM0')
-    dev.initialize()
-    print(dev.idn())
-    dev.set_analog(0, 5000)
-    print(dev.read_analog(0))
-    dev.set_analog(0, 0)
-    values = []
-    for i in range(10):
-        dev.set_analog(0, i*400)
-        values.append(dev.read_analog(0))
-
-    print(values)
+    def read(self):
+        line = self.rsc.readline()
+        line = line.decode(self.DEFAULTS['encoding'])
+        return line.strip(self.DEFAULTS['read_termination'])
