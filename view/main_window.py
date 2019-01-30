7dbe7a388ee103be7d32578e19c6739a208a8bdc
Aquiles Carattino <aquiles@aquicarattino.com>
2019-01-30 16:25:59 +0100

    Status line

--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,23 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+from model.experiment import Experiment
 
+exp = Experiment()
+exp.load_config('experiment.yml')
+exp.load_daq()
 
 app = QApplication([])
 window = QMainWindow()
+uic.loadUi('main_window.ui', window)
+
+def start_press():
+    print('Start Press')
+
+def stop_press():
+    print('Stop Press')
+
+window.start_button.clicked.connect(exp.do_scan)
+window.stop_button.clicked.connect(stop_press)
+
 window.show()
 app.exec_()
