94bbacd3bcafbf4aac97a1182bbd8e7ead52649e
Aquiles Carattino <aquiles@aquicarattino.com>
2019-01-31 10:28:56 +0100

    Updated view

--- view/main_window.py
+++ view/main_window.py
@@ -3,27 +3,29 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 from model.experiment import Experiment
 
-exp = Experiment()
-exp.load_config('experiment.yml')
-exp.load_daq()
 
-app = QApplication([])
-window = QMainWindow()
-uic.loadUi('main_window.ui', window)
+class ScanWindow(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        uic.loadUi('main_window.ui', self)
 
+        self.start_button.clicked.connect(self.start_pressed)
+        self.stop_button.clicked.connect(self.stop_pressed)
 
-def start_press():
-    t = threading.Thread(target=exp.do_scan)
-    t.start()
-    window.status_line.setText('Scan Running')
+    def start_pressed(self):
+        print('Start Pressed')
 
-def stop_press():
-    print('Stop Press')
+    def stop_pressed(self):
+        print('Stop Pressed')
 
 
+if __name__ == "__main__":
+    exp = Experiment()
+    exp.load_config('experiment.yml')
+    exp.load_daq()
 
-window.start_button.clicked.connect(start_press)
-window.stop_button.clicked.connect(stop_press)
 
-window.show()
-app.exec_()
+    app = QApplication([])
+    scan_window = ScanWindow()
+    scan_window.show()
+    app.exec_()
